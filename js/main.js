dc35ce617fbdec1bfde6717eb8e18a64d4904d09
t-lla <tomas.llano.miro@gmail.com>
2025-04-06 20:12:32 +0200

    v0.1 - sidebar + key control + ASCII
    sidebar with functionalities; made accessible with key pressings.
    initial portal + content viewing
    +ASCII WIP

--- js/main.js
+++ js/main.js
@@ -1,18 +1,116 @@
+let currentSection = null;
+let selectedIndex = 0;
+const treeItems = Array.from(document.querySelectorAll("#treeNav li"));
+
 function mainMenu() {
-    document.querySelector('header').classList.remove('hidden');
-    document.getElementById('app').classList.remove('hidden');
+    document.getElementById('portalScreen').classList.remove('hidden');
+    document.getElementById('content').classList.add('hidden');
+    currentSection = null;
 }
 
 function navigateToApp() {
-    document.querySelector('header').classList.add('hidden');
-    document.getElementById('app').classList.remove('hidden');
-    loadSection('habits');
+    document.getElementById('portalScreen').classList.add('hidden');
+    document.getElementById('content').classList.remove('hidden');
+
+    if (currentSection === null) {
+        document.getElementById('content').innerHTML = 
+        `<p>> now what? </p>`;
+    }
 }
 
 function loadSection(section) {
     const content = document.getElementById('content');
-    fetch(`js/${section}.js`)
+
+    if (currentSection === section) {
+        return;
+    }
+
+    const ascii = `
+        
+                                        
+                               *#***#=                               
+                              *::-==::*                              
+                              *:--==-:*                              
+                             #::-====::#                             
+                             *:--==+=-:#                             
+                            %::-======::%                            
+                            . ======+== .                            
+                           .             .                           
+                           .             .                           
+                          .           .   :                          
+                         .            ..  ..                         
+                         :            ..   :                         
+                        *:: .         ... :=#                        
+                        #:-=========+++*+=::%                        
+                       %::===========++*+=-::%                       
+                       %:-=+====-====++**+=::%                       
+                      %::-==========+++***=-::%                      
+                      %:-=+======-===+++**+=-:%                      
+                     : ===+==========++****=-- :                     
+                     : .::   =+=====++*+ ..::. :.                    
+                    :  :::            .....::.  :                    
+                   :: .::.            .....:::. ::                   
+                   :  :::             .....:::.  :                   
+                  -  .:::             .....::::. :-                  
+                 +%:.:::              .....::::: :%+                 
+             =+*+%::-=+*-             .....:=*+=-:+%+**=             
+          ****+**%::=+**==============+++******==::%*++****          
+      :*******++%.:-=+*=========-=====+++******+=-::%+********-      
+   :************%::==**=========-=====++++******==::%**+*********:   
+   #*********+++.:-=+**===============+++*******+=-::*+**********#   
+   %%%=*********%===**==========-=====+++********===%*+*******+%%%   
+    %%%%%+****++*###*-==========-=====++++*****-*###*+*****+%%%%%    
+       #%%%##-**+***********:.         .:***********+*+-**#%%#       
+          *##***:***********************************:#*****          
+              **###*-***************************=#####*              
+                 *#####.++*****************+*.%####*                 
+                     ####%*=+++++++++++++=*%##%*                     
+                        *#%%%% +==+===.%%%%#*                        
+                           :#%##**+**##%#-                           
+                               #**++*#                               
+                                                                     
+                                                                     
+    `;
+
+    content.innerHTML = `<pre>${ascii}</pre><p>> ${section.toUpperCase()} selected.</p>`;
+    content.classList.remove('hidden');
+    currentSection = section;
 }
 
 
-  
+function highlightSelection(index) {
+    treeItems.forEach((item, i) => {
+        item.classList.toggle("selected", i === index);
+    });
+    selectedIndex = index;
+}
+
+highlightSelection(selectedIndex);
+
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowDown") {
+        selectedIndex = (selectedIndex + 1) % treeItems.length;
+        highlightSelection(selectedIndex);
+    } else if (e.key === "ArrowUp") {
+        selectedIndex = (selectedIndex - 1 + treeItems.length) % treeItems.length;
+        highlightSelection(selectedIndex);
+    } else if (e.key === "Enter") {
+        const selectedItem = treeItems[selectedIndex];
+        const section = selectedItem.dataset.section;
+        loadSection(section);
+    }
+});
+
+
+treeItems.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        highlightSelection(index);
+        const section = item.dataset.section;
+        loadSection(section);
+    });
+
+    item.addEventListener("mouseover", () => {
+        highlightSelection(index);
+    });
+});
